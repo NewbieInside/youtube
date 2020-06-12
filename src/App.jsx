ea448c55280636ebc6cfe90c3985a32083fe171a
NewbieInside <shabanov.konstantin@yahoo.com>
2020-06-12 16:06:35 +0300

    google api added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 
+
 import SearchBar from './Components/SearchBar/SearchBar';
+import youtube from './apis/Youtube';
 
 import './App.css';
 
 class App extends Component {
 
 
+
+  onTermSubmit = (term) => {
+    const KEY = 'AIzaSyDQxkP55Mnu7bzd3wQR5dle_XNl077YGiw';
+    youtube.get('/search', {
+      params: {
+        q: term,
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY,
+      },
+    });
+  };
+
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
-    )
+    );
   }
 }
 
 export default App;
-
