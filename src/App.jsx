210989f001707ab92b1d89cd794c0207d99bc10c
NewbieInside <shabanov.konstantin@yahoo.com>
2020-06-12 14:10:52 +0300

    searchbar added plus axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import SearchBar from './Components/SearchBar/SearchBar';
+
 import './App.css';
 
 class App extends Component {
@@ -8,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello React</h1>
+        <SearchBar />
       </div>
     )
   }
---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import styles from './SearchBar.module.css';
+
+class SearchBar extends Component {
+  state = {
+    term: '',
+  };
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value })
+  }
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    
+  }
+
+  render() {
+    return (
+      <div className={styles.searchContainer}>
+        <form onSubmit={this.onFormSubmit} className={styles.searchForm}>
+          <div className={styles.searchBar}>
+            <label htmlFor="videoSearch">
+              <h3>Video Search</h3>
+            </label>
+            <input
+              className={styles.searchInput}
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+          <hr className={styles.searchHr} />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
